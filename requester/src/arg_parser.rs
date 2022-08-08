813a2244f4f82a438bad62e654a5867082486e8d
OLoKo64 <naldoepingo1@gmail.com>
2022-08-07 21:42:45 -0300

    feat: remove lto and reduce default pool size

--- requester/src/arg_parser.rs
+++ requester/src/arg_parser.rs
@@ -9,8 +9,8 @@ pub struct Args {
     pub request_number: u32,
 
     /// Pool size to be used to make the requests.
-    /// Maximum value recommended is 8000.
-    #[clap(short, long, value_parser, default_value_t = 5000)]
+    /// Maximum value recommended is 5000.
+    #[clap(short, long, value_parser, default_value_t = 500)]
     pub pool_size: u32,
 
     /// URL to be used to make the requests.
