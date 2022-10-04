4450f51267b7725c287e4138b104d790b809c424
OLoKo64 <naldoepingo1@gmail.com>
2022-10-04 16:21:05 -0300

    feat: add binary converter

---
+++ byte-converter/src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs;
+
+fn read_file(path: &str) -> String {
+    let file = fs::read_to_string(path).expect("Unable to read input file");
+    file
+}
+
+fn convert_string_to_binary(text: String) -> String {
+    let mut binary = String::new();
+    for character in text.clone().into_bytes() {
+        binary += &format!("0{:b} ", character);
+    }
+    binary.trim().to_string()
+}
+
+fn write_file(path: &str, text: String) {
+    fs::write(path, text).expect("Unable to write output file");
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<_>>();
+
+    if args.len() != 2 {
+        println!("Usage: {} <input-file>", args[0]);
+        return;
+    }
+
+    let output = args[1].split(".").collect::<Vec<_>>()[0].to_string() + "-binary.txt";
+    write_file(&output, convert_string_to_binary(read_file(&args[1])));
+
+    println!("Done!, output file: {}", output);
+}
