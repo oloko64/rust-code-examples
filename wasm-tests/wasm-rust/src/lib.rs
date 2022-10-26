88bb5427bfef57ea242c213012b72fcd7caa9549
OLoKo64 <naldoepingo1@gmail.com>
2022-10-26 12:51:17 -0300

    feat: add more wasm examples

--- wasm-tests/wasm-rust/src/lib.rs
+++ wasm-tests/wasm-rust/src/lib.rs
@@ -1,8 +1,27 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub struct Testing {
+    sum: String,
+    b: Vec<u32>,
+}
+
+#[wasm_bindgen]
+impl Testing {
+    #[wasm_bindgen(getter)]
+    pub fn a(&self) -> String {
+        self.sum.clone()
+    }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_id(&mut self, sum: String) {
+        self.sum = sum;
+    }
+}
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> Testing {
+    Testing { sum: a.to_string(), b: vec![a as u32, b as u32] }
 }
 
 #[wasm_bindgen]
