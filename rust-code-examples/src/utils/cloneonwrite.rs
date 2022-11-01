dfa913327c13509701b0c7a0ce56550956cb3ecc
OLoKo64 <naldoepingo1@gmail.com>
2022-11-01 09:54:16 -0300

    feat: add one more cow example

--- rust-code-examples/src/utils/cloneonwrite.rs
+++ rust-code-examples/src/utils/cloneonwrite.rs
@@ -18,3 +18,31 @@ pub fn run_cow() {
         }
     }
 }
+
+/// Official example from https://doc.rust-lang.org/std/borrow/enum.Cow.html
+pub fn rust_cow_use_example() {
+    // No clone occurs because `input` doesn't need to be mutated.
+    let slice = [0, 1, 2];
+    let mut input = Cow::from(&slice[..]);
+    abs_all(&mut input);
+
+    // Clone occurs because `input` needs to be mutated.
+    let slice = [-1, 0, 1];
+    let mut input = Cow::from(&slice[..]);
+    abs_all(&mut input);
+
+    // No clone occurs because `input` is already owned.
+    let mut input = Cow::from(vec![-1, 0, 1]);
+    abs_all(&mut input);
+}
+
+/// Returns absolute values inside a cloned array if necessary.
+fn abs_all(input: &mut Cow<[i32]>) {
+    for i in 0..input.len() {
+        let v = input[i];
+        if v < 0 {
+            // Clones into a vector if not already owned.
+            input.to_mut()[i] = -v;
+        }
+    }
+}
