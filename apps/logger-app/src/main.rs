0f9e99b289417043b10c24c9761bd62d4cbec977
OLoKo64 <naldoepingo1@gmail.com>
2023-02-27 11:15:42 -0300

    feat: add env-filter

--- apps/logger-app/src/main.rs
+++ apps/logger-app/src/main.rs
@@ -1,7 +1,12 @@
 use std::error::Error;
 
 use time::{macros::format_description, UtcOffset};
-use tracing_subscriber::fmt::{time::OffsetTime, writer::MakeWriterExt};
+use tracing::metadata::LevelFilter;
+use tracing_subscriber::{
+    fmt::{time::OffsetTime, writer::MakeWriterExt, Layer},
+    prelude::*,
+    EnvFilter,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let offset = UtcOffset::current_local_offset()?;
@@ -13,14 +18,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (non_blocking_file, _guard) = tracing_appender::non_blocking(appender);
     let (non_blocking_stdout, _guard_stdout) = tracing_appender::non_blocking(std::io::stdout());
     let all_logs = non_blocking_file.and(non_blocking_stdout);
-
-    // We can initialize the subscriber directly
-    tracing_subscriber::fmt()
+    let layer = Layer::new()
         .with_timer(timer)
         .with_writer(all_logs)
-        .with_ansi(false)
-        .with_max_level(tracing::Level::INFO)
+        .with_ansi(false);
+
+    // We can initialize the subscriber directly
+    tracing_subscriber::registry()
+        .with(layer)
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
         .init();
+
+    // Using tracing_subscriber::fmt() is also possible
+    // tracing_subscriber::fmt()
+    //     .with_timer(timer)
+    //     .with_writer(all_logs)
+    //     .with_ansi(false)
+    //     .with_max_level(tracing::Level::INFO)
+    //     .init();
 
     // Or we can use set_global_default subscriber
     // let subscriber = tracing_subscriber::fmt()
