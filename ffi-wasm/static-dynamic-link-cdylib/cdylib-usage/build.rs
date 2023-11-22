680cb34699828773d642db362b5e9520a4fc5713
OLoKo64 <naldoepingo1@gmail.com>
2023-11-22 14:49:16 -0300

    feat: add doc

--- ffi-wasm/static-dynamic-link-cdylib/cdylib-usage/build.rs
+++ ffi-wasm/static-dynamic-link-cdylib/cdylib-usage/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // https://doc.rust-lang.org/stable/cargo/reference/build-scripts.html#rustc-link-lib
     println!("cargo:rustc-link-search=/home/oloko64/Documents/git-repositories/rust-apps/rust-code-tests/ffi-wasm/static-dynamic-link-cdylib/cdylib-example/target/release");
     println!("cargo:rustc-link-lib=static=cdylib_example");
 }
