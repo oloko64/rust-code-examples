5f5b188bc9ecb98a2046ce0670494624b799c912
OLoKo64 <naldoepingo1@gmail.com>
2023-11-26 19:42:47 -0300

    feat: add vtable example

--- rust-code-examples/src/trait_examples/phantom_data.rs
+++ rust-code-examples/src/trait_examples/phantom_data.rs
@@ -1,5 +1,5 @@
 trait PhantomExample {
-    // A trait method that has a foo method that is not has `&self` as the first argument
+    // A trait that has a foo method that is not has `&self` as the first argument
     // So we cannot call it like `struct.foo(&val)` but rather `Struct::foo(&val)`
     fn foo(x: &str) -> Self;
 }
@@ -25,9 +25,9 @@ impl<T: PhantomExample> PhantomStruct<T> {
     }
 }
 
-struct MyStruct3;
+struct MyStruct;
 
-impl PhantomExample for MyStruct3 {
+impl PhantomExample for MyStruct {
     fn foo(x: &str) -> Self {
         println!("x = {}", x);
         Self
@@ -38,7 +38,69 @@ impl PhantomExample for MyStruct3 {
 // Example: https://www.youtube.com/watch?v=ub5vIPNMqI4
 pub fn example() {
     // Here we can pass the struct as a generic type, not as a value
-    let value = PhantomStruct::<MyStruct3>::new("Hello World".to_string());
+    let value = PhantomStruct::<MyStruct>::new("Hello World".to_string());
+
+    value.run();
+}
+
+// ------------------------------------------ Using trait objects with vtable ------------------------------------------------------
+
+trait PhantomExample2 {
+    // A trait that has a foo method that is not has `&self` as the first argument
+    // So we cannot call it like `struct.foo(&val)` but rather `Struct::foo(&val)`
+    // We also need to make the trait object safe by adding the Sized trait bound
+    fn foo(x: &str) -> Self
+    where
+        Self: Sized;
+
+    // Here we added a method that is on the vtable
+    fn bar(&self) -> String;
+}
+
+// Here we have to use a PhantomData marker to tell the compiler that we are using the generic
+struct PhantomStruct2<'a, T: PhantomExample2> {
+    name: String,
+
+    // Now we need to add a reference to the trait object, so we can have access to it's vtable methods
+    struct_val: &'a dyn PhantomExample2,
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<'a, T: PhantomExample2> PhantomStruct2<'a, T> {
+    fn new(val: &'a dyn PhantomExample2) -> Self {
+        Self {
+            name: String::from("hello"),
+            struct_val: val,
+            _marker: std::marker::PhantomData,
+        }
+    }
+
+    // Here we can call the foo method on the generic type even though it is not on the vtable
+    fn run(&self) {
+        // Now we can call the bar method on the vtable
+        let _ = self.struct_val.bar();
+
+        T::foo(&self.name);
+    }
+}
+
+struct MyStruct2;
+
+impl PhantomExample2 for MyStruct2 {
+    fn foo(x: &str) -> Self {
+        println!("x = {}", x);
+        Self
+    }
+
+    fn bar(&self) -> String {
+        String::from("hello")
+    }
+}
+
+pub fn example_2() {
+    // Now we have no pass the struct to the function so it can have access to it
+    let struct_val = MyStruct2;
+    let value = PhantomStruct2::<MyStruct2>::new(&struct_val);
 
     value.run();
 }
