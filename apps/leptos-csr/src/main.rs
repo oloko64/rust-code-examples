ba0a27676c970a6aac2e9886993fe18b08c946af
OLoKo64 <naldoepingo1@gmail.com>
2024-02-01 13:52:27 -0300

    feat: add request example

--- apps/leptos-csr/src/main.rs
+++ apps/leptos-csr/src/main.rs
@@ -3,10 +3,28 @@ use leptos::*;
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let res = create_local_resource(|| (), |_| async { fetch_data().await });
 
     view! {
         class="container",
         <p>COUNTER:</p>
+        <Suspense fallback=move || view! {<p>"Loading Github"</p> }>
+            {
+                move || {
+                    // Normally you would use a <ErrorBoundary> here, but Result<String, String> have trait bound issues
+                    res.get().map(|data| {
+                        match data {
+                            Ok(data) => view! {
+                                <p>{data}</p>
+                            },
+                            Err(data) => view! {
+                                <p>Err: {data}</p>
+                            }
+                        }
+                    })
+                }
+            }
+        </Suspense>
         <button class="one"
             on:click=move |_| {
                 let val = ReadSignal::get(&count);
@@ -20,6 +38,22 @@ fn App() -> impl IntoView {
             "Click me: "
             {move || count.get()}
         </button>
+    }
+}
+
+async fn fetch_data() -> Result<String, String> {
+    let client = reqwest::Client::new();
+    let res = client
+        .get("https://api.github.com")
+        .send()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    if res.status().is_success() {
+        let body = res.text().await.map_err(|e| e.to_string())?;
+        Ok(body)
+    } else {
+        Err("Failed to fetch data".to_string())
     }
 }
 
